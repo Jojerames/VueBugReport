3e0bfab0cccda1e1bace0b53eb43a4bb12e9119b
jojeramesname <jojeramesname@gmail.com>
2025-11-07 16:27:32 -0700

    added first code

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,22 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
+<!-- The following has the indent issue when adding a new line -->
+<script setup lang="ts">
+    function helloWorld() {
+        console.log("Hello, world!");
+    }
+</script>
+
+<!-- The following does NOT have the indent issue when adding a new line -->
+<script
+    setup
+    lang="ts">
+    function helloWorld() {
+        console.log("Hello, world!");
+    }
+</script>
+
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
